6d1a6abfe52d04e1f66f3caecff948a387d3ccb8
8dcc <8dcc.git@gmail.com>
2022-12-29 01:42:04 +0100

    Remove alloc test 2 from kernel_main

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -88,8 +88,6 @@ void kernel_main(Multiboot* multiboot_info) {
     TEST_TITLE("\nInit header and alloc tests");
     init_heap();
     void* test1 = malloc(255);
-    void* test2 = malloc(100);
-    free(test2);
     free(test1);
 
     test_libk();
