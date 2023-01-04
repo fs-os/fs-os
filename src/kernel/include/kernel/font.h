c56c3a1679be4150113ea081e5554a9858df9dd0
8dcc <8dcc.git@gmail.com>
2023-01-04 21:28:50 +0100

    Add font header and main font

---
+++ src/kernel/include/kernel/font.h
@@ -0,0 +1,26 @@
+
+#ifndef _KERNEL_FONT_H
+#define _KERNEL_FONT_H
+
+#include <stdint.h>
+
+typedef struct {
+    uint8_t w;
+    uint8_t h;
+    uint8_t* font; /* 256 chars * w */
+} Font;
+
+/* get_font_bit: Gets the bit at pos (y, x) of the char "c" from the Font "font".
+   Would be the same as font->font[c][y][x] if it was a 3d array. For more info see:
+   src/kernel/include/fonts/main_font.h */
+static inline uint8_t get_font_bit(Font* font, uint8_t c, uint8_t y, uint8_t x) {
+    if (y > font->h)
+        y = font->h;
+    if (x > font->w)
+        x = font->w;
+
+    return font->font[c * font->w + x] & (1 << y);
+}
+
+#endif /* _KERNEL_FONT_H */
+
