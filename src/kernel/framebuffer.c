4302df89c6ccf418eb93fb30f7fa73850cf71c33
8dcc <8dcc.git@gmail.com>
2023-01-06 01:17:24 +0100

    Move RGB wrappers to the header as inline functions

--- src/kernel/framebuffer.c
+++ src/kernel/framebuffer.c
@@ -1,6 +1,5 @@
 
 #include <kernel/framebuffer.h>
-#include <kernel/color.h>
 
 /* Framebuffer globals */
 static uint32_t* g_fb; /* We will save the pointer, not a copy of the framebuffer */
@@ -25,6 +24,7 @@ void fb_init(uint32_t* fb, uint32_t pitch, uint32_t w, uint32_t h, uint32_t bpp)
     }
 }
 
+/* fb_setpx: RGB wrapper for fb_setpx_col */
 /* fb_setpx_col: set the pixel at (y, x) of the global framebuffer to "col" */
 void fb_setpx_col(uint32_t y, uint32_t x, uint32_t col) {
     if (y >= g_height || x >= g_width)
@@ -33,13 +33,7 @@ void fb_setpx_col(uint32_t y, uint32_t x, uint32_t col) {
     g_fb[y * g_width + x] = col;
 }
 
-/* fb_setpx: set the pixel at (y, x) of the global framebuffer to the converted
- * color from "r", "g" and "b".
- * TODO: Move wrappers to header as static inline? */
-void fb_setpx(uint32_t y, uint32_t x, uint8_t r, uint8_t g, uint8_t b) {
-    fb_setpx_col(y, x, rgb2col(r, g, b));
-}
-
+/* fb_drawrect: RGB wrapper for fb_drawrect_col */
 /* fb_drawrect_col: fill "h" and "w" starting from "y" and "x" with color "col" */
 void fb_drawrect_col(uint32_t y, uint32_t x, uint32_t h, uint32_t w, uint32_t col) {
     if (y >= g_height || x >= g_width)
@@ -54,11 +48,5 @@ void fb_drawrect_col(uint32_t y, uint32_t x, uint32_t h, uint32_t w, uint32_t co
     for (uint32_t cur_y = y; cur_y < y + h; cur_y++)
         for (uint32_t cur_x = x; cur_x < x + w; cur_x++)
             g_fb[cur_y * g_width + cur_x] = col;
-}
-
-/* fb_drawrect: RGB wrapper for fb_drawrect_col */
-void fb_drawrect(uint32_t y, uint32_t x, uint32_t h, uint32_t w, uint8_t r,
-                 uint8_t g, uint8_t b) {
-    fb_drawrect_col(y, x, h, w, rgb2col(r, g, b));
 }
 
--- src/kernel/include/kernel/framebuffer.h
+++ src/kernel/include/kernel/framebuffer.h
@@ -3,6 +3,7 @@
 #define _KERNEL_FRAMEBUFFER_H
 
 #include <stdint.h>
+#include <kernel/color.h>
 
 /*
  * See bottom of:
@@ -20,16 +21,19 @@ void fb_init(uint32_t* fb, uint32_t pitch, uint32_t w, uint32_t h, uint32_t bpp)
 /* fb_setpx_col: set the pixel at (y, x) of the global framebuffer to "col" */
 void fb_setpx_col(uint32_t y, uint32_t x, uint32_t col);
 
-/* fb_setpx: set the pixel at (y, x) of the global framebuffer to the converted
- * color from "r", "g" and "b".
- * TODO: Move wrappers to header as static inline? */
-void fb_setpx(uint32_t y, uint32_t x, uint8_t r, uint8_t g, uint8_t b);
+/* fb_setpx: RGB wrapper for fb_setpx_col */
+static inline void fb_setpx(uint32_t y, uint32_t x, uint8_t r, uint8_t g,
+                            uint8_t b) {
+    fb_setpx_col(y, x, rgb2col(r, g, b));
+}
 
 /* fb_drawrect_col: fill "h" and "w" starting from "y" and "x" with color "col" */
 void fb_drawrect_col(uint32_t y, uint32_t x, uint32_t h, uint32_t w, uint32_t col);
 
 /* fb_drawrect: RGB wrapper for fb_drawrect_col */
-void fb_drawrect(uint32_t y, uint32_t x, uint32_t h, uint32_t w, uint8_t r,
-                 uint8_t g, uint8_t b);
+static inline void fb_drawrect(uint32_t y, uint32_t x, uint32_t h, uint32_t w,
+                               uint8_t r, uint8_t g, uint8_t b) {
+    fb_drawrect_col(y, x, h, w, rgb2col(r, g, b));
+}
 
 #endif /* _KERNEL_FRAMEBUFFER_H */
