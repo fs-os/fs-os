9814170053c9f1774930f2ab996c807b10f6f551
8dcc <8dcc.git@gmail.com>
2023-01-16 17:50:24 +0100

    Move PIT defines to header, add comments

--- src/kernel/include/kernel/pit.h
+++ src/kernel/include/kernel/pit.h
@@ -4,6 +4,43 @@
 
 #include <stdint.h>
 
+/* How many times the PIT oscilates in 1 sec by default */
+#define PIT_BASE_FREQ 1193182
+
+/* Frequency in 1 sec (e.g. 1000 times in a sec == 1ms) to PIT hz */
+#define PIT_INTERVAL_TO_FREQ(freq) (PIT_BASE_FREQ / freq)
+
+enum pit_io_ports {
+    PIT_CHANNEL_0   = 0x40, /* time.h functions (sleep, etc.) */
+    PIT_CHANNEL_1   = 0x41, /* unused */
+    PIT_CHANNEL_2   = 0x42, /* pc speaker */
+    PIT_CHANNEL_CMD = 0x43, /* configuring frequencies */
+};
+
+enum pit_cmd_flags {
+    PIT_FLAG_CHANNEL_0 = 0x00, /* 00000000 */
+    PIT_FLAG_CHANNEL_1 = 0x40, /* 01000000 */
+    PIT_FLAG_CHANNEL_2 = 0x80, /* 10000000 */
+    PIT_FLAG_CHANNEL_3 = 0xC0, /* 11000000 */
+
+    PIT_FLAG_ACCESS_COUNT = 0x00, /* 00000000 */
+    PIT_FLAG_ACCESS_LO    = 0x10, /* 00010000 */
+    PIT_FLAG_ACCESS_HI    = 0x20, /* 00100000 */
+    PIT_FLAG_ACCESS_LOHI  = 0x30, /* 00110000 */
+
+    PIT_FLAG_MODE_INT       = 0x00, /* 00000000 */
+    PIT_FLAG_MODE_HW_RET    = 0x02, /* 00000010 */
+    PIT_FLAG_MODE_SQUARE    = 0x04, /* 00000100 */
+    PIT_FLAG_MODE_RATE      = 0x06, /* 00000110 */
+    PIT_FLAG_MODE_SW_STROBE = 0x08, /* 00001000 */
+    PIT_FLAG_MODE_HW_STROBE = 0x0A, /* 00001010 */
+    PIT_FLAG_MODE_RATE_2    = 0x0C, /* 00001100. Same as rate */
+    PIT_FLAG_MODE_SQUARE_2  = 0x0E, /* 00001110. Same as square */
+
+    PIT_FLAG_BINARY_OFF = 0x0, /* 00000000 */
+    PIT_FLAG_BINARY_BCD = 0x1, /* 00000001 */
+};
+
 /* pit_init: initialize the programmable interval timer with the specified frequency
  * in interrupts per seccond (freq 1000 would mean 1000 ticks in 1 sec) */
 void pit_init(uint32_t freq);
@@ -21,3 +58,4 @@ void pit_set_ticks(uint64_t num);
 uint64_t pit_get_ticks(void);
 
 #endif /* _KERNEL_PIT_H */
+
--- src/kernel/pit.c
+++ src/kernel/pit.c
@@ -4,45 +4,13 @@
 #include <kernel/pit.h>
 #include <kernel/io.h>
 
-/* How many times the PIT oscilates in 1 sec by default */
-#define BASE_FREQ 1193182
-
-enum pit_io_ports {
-    PIT_CHANNEL_0   = 0x40,
-    PIT_CHANNEL_1   = 0x41,
-    PIT_CHANNEL_2   = 0x42,
-    PIT_CHANNEL_CMD = 0x43,
-};
-
-enum pit_cmd_flags {
-    PIT_FLAG_CHANNEL_0 = 0x00, /* 00000000 */
-    PIT_FLAG_CHANNEL_1 = 0x40, /* 01000000 */
-    PIT_FLAG_CHANNEL_2 = 0x80, /* 10000000 */
-    PIT_FLAG_CHANNEL_3 = 0xC0, /* 11000000 */
-
-    PIT_FLAG_ACCESS_COUNT = 0x00, /* 00000000 */
-    PIT_FLAG_ACCESS_LO    = 0x10, /* 00010000 */
-    PIT_FLAG_ACCESS_HI    = 0x20, /* 00100000 */
-    PIT_FLAG_ACCESS_LOHI  = 0x30, /* 00110000 */
-
-    PIT_FLAG_MODE_INT       = 0x00, /* 00000000 */
-    PIT_FLAG_MODE_HW_RET    = 0x02, /* 00000010 */
-    PIT_FLAG_MODE_SQUARE    = 0x04, /* 00000100 */
-    PIT_FLAG_MODE_RATE      = 0x06, /* 00000110 */
-    PIT_FLAG_MODE_SW_STROBE = 0x08, /* 00001000 */
-    PIT_FLAG_MODE_HW_STROBE = 0x0A, /* 00001010 */
-    PIT_FLAG_MODE_RATE_2    = 0x0C, /* 00001100. Same as rate */
-    PIT_FLAG_MODE_SQUARE_2  = 0x0E, /* 00001110. Same as square */
-
-    PIT_FLAG_BINARY_OFF = 0x0, /* 00000000 */
-    PIT_FLAG_BINARY_BCD = 0x1, /* 00000001 */
-};
-
 /* pit_init: initialize the programmable interval timer with the specified frequency
  * in interrupts per seccond (freq 1000 would mean 1000 ticks in 1 sec) */
 void pit_init(uint32_t freq) {
-    /* freq should be at what tick it should send an interrupt */
-    freq = BASE_FREQ / freq;
+    /* freq should be how many HZs it should wait bewteen sending interrupt. We pass
+     * the frequency per second to convert it to HZ (by dividing how many HZs are in
+     * a sec) */
+    freq = PIT_INTERVAL_TO_FREQ(freq);
 
     /* Select mode/cmd and flags */
     io_outb(PIT_CHANNEL_CMD, PIT_FLAG_CHANNEL_0 | PIT_FLAG_ACCESS_LOHI |
