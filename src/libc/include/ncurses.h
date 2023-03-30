3a0626853967e749786be79d0998205fcec353d3
8dcc <8dcc.git@gmail.com>
2023-03-30 11:44:52 +0200

    Add ncurses header with TODO comment

---
+++ src/libc/include/ncurses.h
@@ -0,0 +1,36 @@
+
+#ifndef _NCURSES_H
+#define _NCURSES_H 1
+
+/*
+ * TODO:
+ *  - initscr
+ *  - raw       (add kb_raw)
+ *  - noecho    (kb_noecho)
+ *  - endwin
+ *  - refresh   (fbc_refresh, no ncurses buffer)
+ *  - move
+ *  - getyx     (2 arg version and with no macro)
+ *  - printw    (printf)
+ *  - mvprintw  (move & printf)
+ *  - vprintw   (vprintf)
+ *  - addch     (putchar)
+ *  - mvaddch   (move & putchar)
+ *  - clrtoeol  (clear to end of line)
+ *  - getch     (getchar)
+ *
+ * ERROR:
+ *  - wprintw   (window printf)
+ *  - mvwprintw (move & window printf)
+ *  - waddch    (window putchar)
+ *  - mvwaddch  (move & window putchar)
+ *  - keypad    (arrow keys, numpad)
+ *  - mousemask (cursor)
+ *  - getmouse
+ *  - has_colors
+ *  - start_color
+ *  - init_pair
+ */
+
+#endif /* _NCURSES_H */
+
