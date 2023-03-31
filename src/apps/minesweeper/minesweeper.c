e659d1528d60ca8c5f89a8b7f5996284f8488580
8dcc <8dcc.git@gmail.com>
2023-03-31 23:45:18 +0200

    Add initial curses window test to minesweeper_main

--- src/apps/minesweeper/minesweeper.c
+++ src/apps/minesweeper/minesweeper.c
@@ -1,13 +1,22 @@
 
 #include <stdio.h>
-#include <ncurses.h>
+#include <curses.h>
 
 /* minesweeper_main: entry point of the minesweeper */
 int minesweeper_main(int argc, char** argv) {
-    puts("start");
-    /* initscr(); */
+    /* TODO */
+    (void)argc;
+    (void)argv;
 
-    /* endwin(); */
+    puts("start");
+    WINDOW* stdscr = initscr();
+
+    puts("Hello, curses window!\nPress any key to continue...");
+
+    while (getchar() != '\n')
+        ;
+
+    endwin(stdscr);
     puts("done");
 
     return 0;
