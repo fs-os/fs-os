194b36477f55ce8df42d76cab3f07b2f6a17b3c0
8dcc <8dcc.git@gmail.com>
2023-04-04 00:56:22 +0200

    Remove old minesweeper folder

--- src/apps/minesweeper/minesweeper.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _APPS_MINESWEEPER_H
-#define _APPS_MINESWEEPER_H 1
-
-/* minesweeper_main: entry point of the minesweeper */
-int minesweeper_main(int argc, char** argv);
-
-#endif /* _APPS_MINESWEEPER_H */
-
