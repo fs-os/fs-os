d1994691e053411950fbe162a6dca4b24e617716
8dcc <8dcc.git@gmail.com>
2023-04-17 16:44:22 +0200

    Revert new year method

--- src/libc/time.c
+++ src/libc/time.c
@@ -4,29 +4,11 @@
 #include <kernel/pit.h>
 #include <kernel/rtc.h>
 
-#define MIN2SEC(x)  (x * 60)
-#define HOUR2SEC(x) (x * 3600)
-#define DAY2SEC(x)  (x * 86400)
-#define MON2SEC(x)  (x * 2629743)
-
-/**
- * @def YEAR2SEC
- * @brief Generates seconds that passed since epoch to 1/1/x, x being a year
- * minus 1900 (1975 -> 75; 2023 -> 123)
- *
- * @details It adds in a day for each year that follows a leap year starting
- * with the first leap year since the Epoch. The first term adds a day every 4
- * years starting in 1973, the second subtracts a day back out every 100 years
- * starting in 2001, and the third adds a day back in every 400 years starting
- * in 2001. The divisions in the formula are integer divisions; that is, the
- * remainder is discarded leaving only the integer quotient.
- *
- * Source:
- * https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap04.html#tag_04_16
- */
-#define YEAR2SEC(x)                                                           \
-    ((x - 70) * 31536000 + ((x - 69) / 4) * 86400 - ((x - 1) / 100) * 86400 + \
-     ((x + 299) / 400) * 86400)
+#define MIN2SEC(x)  ((x)*60)
+#define HOUR2SEC(x) ((x)*3600)
+#define DAY2SEC(x)  ((x)*86400)
+#define MON2SEC(x)  ((x)*2629743)
+#define YEAR2SEC(x) ((x)*31556926)
 
 /** @todo: Needs like 900 million seconds more :) */
 uint32_t time(void* tloc) {
@@ -40,7 +22,7 @@ uint32_t time(void* tloc) {
     const uint32_t hour = HOUR2SEC(now.time.h);
     const uint32_t day  = DAY2SEC(now.date.d);
     const uint32_t mon  = MON2SEC(now.date.m);
-    const uint32_t year = YEAR2SEC(now.date.y + 100); /* (years since 1900) */
+    const uint32_t year = YEAR2SEC(now.date.y + 30); /* (years since 1970) */
 
     return sec + min + hour + day + mon + year;
 }
