8b8909e347e2f3f6f6be5034c6346e5aca6cb7e6
debooger <debooger@debooger.xyz>
2023-05-15 22:52:48 +0200

    Fixed assembly math functions

--- src/libc/math.asm
+++ src/libc/math.asm
@@ -4,56 +4,64 @@ section .text
 sqrt:
     push ebp
     mov ebp, esp
+    sub esp, 8
 
     movsd xmm0, qword [ebp + 8]
     sqrtsd xmm0, xmm0
-    movsd qword [ebp + 8], xmm0
+    movsd qword [ebp - 8], xmm0
+    fld qword [ebp - 8]
 
-    pop ebp
+    leave
     ret
 
 sin:
     push ebp
     mov ebp, esp
+    sub esp, 8
 
     fld qword [ebp + 8]
     fsin
-    fstp qword [ebp + 8]
+    fstp qword [ebp - 8]
+    fld qword [ebp - 8]
 
-    pop ebp
+    leave
     ret
 
 cos:
     push ebp
     mov ebp, esp
+    sub esp, 8
 
     fld qword [ebp + 8]
     fcos
     fstp qword [ebp + 8]
+    fld qword [ebp - 8]
 
-    pop ebp
+    leave
     ret
 
 tan:
     push ebp
     mov ebp, esp
+    sub esp, 8
 
     fld qword [ebp + 8]
     fsincos
     fdivp st1, st0
     fstp qword [ebp + 8]
 
-    pop ebp
+    leave
     ret
 
 cot:
     push ebp
     mov ebp, esp
+    sub esp, 8
 
     fld qword [ebp + 8]
     fsincos
     fdivrp st1, st0
     fstp qword [ebp + 8]
 
-    pop ebp
+    leave
     ret
