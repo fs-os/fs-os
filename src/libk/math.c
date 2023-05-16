3aa5e1498e04d34fbbd7b71678c9fc23738dd9f1
debooger <debooger@debooger.xyz>
2023-05-16 10:49:29 +0200

    Removed functions including inline assembly

--- src/libk/math.c
+++ src/libk/math.c
@@ -4,64 +4,6 @@ double abs(double x) {
     return x < 0 ? -x : x;
 }
 
-double sqrt(double x) {
-    double result;
-
-    asm volatile("sqrtsd %0, %1" : "=x" (result) : "x" (x));
-
-    return result;
-}
-
-double sin(double x) {
-    double result;
-
-    asm volatile("fld %1\n\t"
-                 "fsin\n\t"
-                 "fstp %0"
-                 : "=m" (result)
-                 : "m" (x));
-
-    return result;
-}
-
-double cos(double x) {
-    double result;
-
-    asm volatile("fld %1\n\t"
-                 "fcos\n\t"
-                 "fstp %0"
-                 : "=m" (result)
-                 : "m" (x));
-
-    return result;
-}
-
-double tan(double x) {
-    double result;
-
-    asm volatile("fld %1\n\t"
-                 "fsincos\n\t"
-                 "fdivp\n\t"
-                 "fstp %0"
-                 : "=m" (result)
-                 : "m" (x));
-
-    return result;
-}
-
-double cot(double x) {
-    double result;
-
-    asm volatile("fld %1\n\t"
-                 "fsincos\n\t"
-                 "fdivrp\n\t"
-                 "fstp %0"
-                 : "=m" (result)
-                 : "m" (x));
-
-    return result;
-}
-
 double pow(double base, int exponent) {
     double result = 1;
 
