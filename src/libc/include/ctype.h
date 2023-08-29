72994bb8e7c37f38d697d9457a24a0b5d47e0b70
8dcc <8dcc.git@gmail.com>
2023-08-29 14:39:09 +0200

    Make isupper and islower inlines instead of macros

--- src/libc/include/ctype.h
+++ src/libc/include/ctype.h
@@ -3,16 +3,22 @@
 #define _CTYPE_H 1
 
 /**
- * @def isupper
  * @brief Returns true if `c` is uppercase.
+ * @param[in] c Character to check
+ * @return True of uppercase
  */
-#define isupper(c) (c >= 'A' && c <= 'Z')
+static inline bool isupper(int c) {
+    return c >= 'A' && c <= 'Z';
+}
 
 /**
- * @def islower
  * @brief Returns true if `c` is lowercase.
+ * @param[in] c Character to check
+ * @return True of lowercase
  */
-#define islower(c) (c >= 'a' && c <= 'z')
+static inline bool islower(c) {
+    return c >= 'a' && c <= 'z';
+}
 
 /**
  * @brief If c is uppercase, returns its lowercase version.
