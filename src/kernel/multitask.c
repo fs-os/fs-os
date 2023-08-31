74847a70ed0a29a0c41f1c9041e1309721bb5e25
8dcc <8dcc.git@gmail.com>
2023-08-31 02:19:20 +0200

    Fix dump_task_list()

--- src/kernel/multitask.c
+++ src/kernel/multitask.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 #include <kernel/multitask.h>
 
+/** @todo Print fxdata */
 void dump_task_list(void) {
     puts("Dumping task list:");
 
     /* 0 will be the current one, not the first task we created */
     int i = 0;
 
-    Ctx* const first_ctx = mt_current_task;
+    Ctx* first_ctx = mt_current_task;
 
     /* We at least want to print one */
     printf("[%d] %s | prev: %p | next: %p | stack: 0x%lX | esp: 0x%lX | cr3: "
@@ -17,7 +18,7 @@ void dump_task_list(void) {
            i++, first_ctx->name, first_ctx->prev, first_ctx->next,
            first_ctx->stack, first_ctx->esp, first_ctx->cr3);
 
-    for (Ctx* cur_ctx = mt_current_task; cur_ctx->next != first_ctx;
+    for (Ctx* cur_ctx = first_ctx->next; cur_ctx != first_ctx;
          cur_ctx      = cur_ctx->next, i++) {
         printf("[%d] %s | prev: %p | next: %p | stack: 0x%lX | esp: 0x%lX | "
                "cr3: 0x%lX\n",
@@ -25,4 +26,3 @@ void dump_task_list(void) {
                cur_ctx->esp, cur_ctx->cr3);
     }
 }
-
