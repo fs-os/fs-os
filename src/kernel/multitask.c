ed8265d4150a46ed131686b6618cc1e7a8522d9b
8dcc <8dcc.git@gmail.com>
2023-08-31 17:32:58 +0200

    Print fxdata in dump_task_list

--- src/kernel/multitask.c
+++ src/kernel/multitask.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <kernel/multitask.h>
 
-/** @todo Print fxdata */
 void dump_task_list(void) {
     puts("Dumping task list:");
 
@@ -13,16 +12,16 @@ void dump_task_list(void) {
     Ctx* first_ctx = mt_current_task;
 
     /* We at least want to print one */
-    printf("[%d] %s | prev: %p | next: %p | stack: 0x%lX | esp: 0x%lX | cr3: "
-           "0x%lX\n",
-           i++, first_ctx->name, first_ctx->prev, first_ctx->next,
-           first_ctx->stack, first_ctx->esp, first_ctx->cr3);
+    printf("[%d] %s | ctx: %p | prev: %p | next: %p | stack: 0x%lX | esp: "
+           "0x%lX | cr3: 0x%lX | fxdata: 0x%lX\n",
+           i++, first_ctx->name, first_ctx, first_ctx->prev, first_ctx->next,
+           first_ctx->stack, first_ctx->esp, first_ctx->cr3, first_ctx->fxdata);
 
     for (Ctx* cur_ctx = first_ctx->next; cur_ctx != first_ctx;
          cur_ctx      = cur_ctx->next, i++) {
-        printf("[%d] %s | prev: %p | next: %p | stack: 0x%lX | esp: 0x%lX | "
-               "cr3: 0x%lX\n",
-               i, cur_ctx->name, cur_ctx->prev, cur_ctx->next, cur_ctx->stack,
-               cur_ctx->esp, cur_ctx->cr3);
+        printf("[%d] %s | ctx: %p | prev: %p | next: %p | stack: 0x%lX | esp: "
+               "0x%lX | cr3: 0x%lX | fxdata: 0x%lX\n",
+               i, cur_ctx->name, cur_ctx, cur_ctx->prev, cur_ctx->next,
+               cur_ctx->stack, cur_ctx->esp, cur_ctx->cr3, cur_ctx->fxdata);
     }
 }
