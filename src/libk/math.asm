2108f8fad50b2da403308ca763857445932be403
8dcc <8dcc.git@gmail.com>
2023-09-07 00:38:37 +0200

    Change coding style of math.asm
    Replace leave's with: mov esp, ebp; pop ebp;
    Change indentation of assembly
    Add comments
    Change location of global's

--- src/libk/math.asm
+++ src/libk/math.asm
@@ -1,67 +1,81 @@
 section .text
-    global sqrt, sin, cos, tan, cot
 
+; double sqrt(double x);
+global sqrt:function
 sqrt:
-    push ebp
-    mov ebp, esp
-    sub esp, 8
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8                      ; Reserve 8 bytes on the stack
 
-    movsd xmm0, qword [ebp + 8]
-    sqrtsd xmm0, xmm0
-    movsd qword [ebp - 8], xmm0
-    fld qword [ebp - 8]
+    movsd   xmm0, qword [ebp + 8]       ; Move double argument to xmm0
+    sqrtsd  xmm0, xmm0                  ; xmm0 = sqrt(xmm0);
+    movsd   qword [ebp - 8], xmm0       ; Move xmm0 to reserved bytes
+    fld     qword [ebp - 8]             ; Push reserved bytes to FPU stack
 
-    leave
+    mov     esp, ebp
+    pop     ebp
     ret
 
+; double sin(double x);
+global sin:function
 sin:
-    push ebp
-    mov ebp, esp
-    sub esp, 8
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8                      ; Reserve 8 bytes on the stack
 
-    fld qword [ebp + 8]
-    fsin
-    fstp qword [ebp - 8]
-    fld qword [ebp - 8]
+    fld     qword [ebp + 8]             ; Push to FPU stack
+    fsin                                ; st(0) = sin(st(0));
+    fstp    qword [ebp - 8]             ; Move st(0) to reserved bytes
+    fld     qword [ebp - 8]             ; Push reserved bytes to FPU stack
 
-    leave
+    mov     esp, ebp
+    pop     ebp
     ret
 
+; double cos(double x);
+global cos:function
 cos:
-    push ebp
-    mov ebp, esp
-    sub esp, 8
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8
 
-    fld qword [ebp + 8]
+    fld     qword [ebp + 8]
     fcos
-    fstp qword [ebp + 8]
-    fld qword [ebp - 8]
+    fstp    qword [ebp + 8]
+    fld     qword [ebp - 8]
 
-    leave
+    mov     esp, ebp
+    pop     ebp
     ret
 
+; double tan(double x);
+global tan:function
 tan:
-    push ebp
-    mov ebp, esp
-    sub esp, 8
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8
 
-    fld qword [ebp + 8]
+    fld     qword [ebp + 8]
     fsincos
-    fdivp st1, st0
-    fstp qword [ebp + 8]
+    fdivp   st1, st0                    ; st(1) /= st(0)
+    fstp    qword [ebp + 8]
 
-    leave
+    mov     esp, ebp
+    pop     ebp
     ret
 
+; double cot(double x);
+global cot:function
 cot:
-    push ebp
-    mov ebp, esp
-    sub esp, 8
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 8
 
-    fld qword [ebp + 8]
+    fld     qword [ebp + 8]
     fsincos
-    fdivrp st1, st0
-    fstp qword [ebp + 8]
+    fdivrp  st1, st0                    ; st(0) = st(1) / st(0)
+    fstp    qword [ebp + 8]
 
-    leave
+    mov     esp, ebp
+    pop     ebp
     ret
