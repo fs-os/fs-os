48b4f81c807c1ababf311a7901a81c861b139c2c
8dcc <8dcc.git@gmail.com>
2023-09-07 19:47:36 +0200

    Add emms instruction to math.asm functions

--- src/libk/math.asm
+++ src/libk/math.asm
@@ -7,6 +7,7 @@ fabs:
     mov     ebp, esp
     sub     esp, 8                      ; Reserve 8 bytes on the stack
 
+    emms                                ; Reset FPU. See Vol.1, Chapter 9.4.8
     fld     qword [ebp + 8]             ; Push to FPU stack
     fabs                                ; st(0) = fabs(st(0));
 
@@ -21,6 +22,7 @@ sqrt:
     mov     ebp, esp
     sub     esp, 8                      ; Reserve 8 bytes on the stack
 
+    emms                                ; Reset FPU. See Vol.1, Chapter 9.4.8
     movsd   xmm0, qword [ebp + 8]       ; Move double argument to xmm0
     sqrtsd  xmm0, xmm0                  ; xmm0 = sqrt(xmm0);
     movsd   qword [ebp - 8], xmm0       ; Move xmm0 to reserved bytes
@@ -37,6 +39,7 @@ sin:
     mov     ebp, esp
     sub     esp, 8
 
+    emms
     fld     qword [ebp + 8]
     fsin                                ; st(0) = sin(st(0));
 
@@ -51,6 +54,7 @@ cos:
     mov     ebp, esp
     sub     esp, 8
 
+    emms
     fld     qword [ebp + 8]
     fcos                                ; st(0) = cos(st(0));
 
@@ -65,6 +69,7 @@ tan:
     mov     ebp, esp
     sub     esp, 8
 
+    emms
     fld     qword [ebp + 8]
     fsincos                             ; st1 = cos(st0); st0 = sin(st0);
     fdivp   st1, st0                    ; st1 /= st0; (sin/cos)
@@ -80,6 +85,7 @@ cot:
     mov     ebp, esp
     sub     esp, 8
 
+    emms
     fld     qword [ebp + 8]
     fsincos                             ; st1 = cos(st0); st0 = sin(st0);
     fdivrp  st1, st0                    ; st0 = st1 / st0; (cos/sin)
