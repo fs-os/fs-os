72ca38d0f3dadeafe143db216e707c449e998465
8dcc <8dcc.git@gmail.com>
2023-09-07 19:31:19 +0200

    Add interactive mode to mandelbrot program

--- src/apps/mandelbrot/mandelbrot.c
+++ src/apps/mandelbrot/mandelbrot.c
@@ -13,7 +13,17 @@
 #include <stdlib.h>
 #include <math.h> /* fabs, fmod */
 
+#include <kernel/keyboard.h>
 #include <kernel/framebuffer.h>
+
+/* Keys */
+#define KEY_ZOOM_IN  '.'
+#define KEY_ZOOM_OUT ','
+#define KEY_UP       'k'
+#define KEY_DOWN     'j'
+#define KEY_LEFT     'h'
+#define KEY_RIGHT    'l'
+#define KEY_RESET    'r'
 
 /* Max HSV values (Used for scaling) */
 #define MAX_H 360
@@ -26,18 +36,22 @@
 #define COL_V 1.0
 
 /* Smaller -> More zoom (1.0 is defaul) */
-#define ZOOM 1.0
+#define DEFAULT_ZOOM 1.0
+#define ZOOM_STEP    0.5
 
 /* Offsets for moving in the set. */
-#define X_OFFSET 0.0 /* -2.0..+2.0 (Left..Right) */
-#define Y_OFFSET 0.0 /* -1.0..+1.0 (Up..Down) */
+#define DEFAULT_X_OFF 0.0 /* -2.0..+2.0 (Left..Right) */
+#define DEFAULT_Y_OFF 0.0 /* -1.0..+1.0 (Up..Down) */
 
 /* RGB color used for drawing the interior of the mandelbrot set */
 #define INSIDE_COL 0x000000
 
 static void hsv2rgb(float* r, float* g, float* b, float h, float s, float v);
 
-/** @todo Use windows once it's added */
+/**
+ * @todo Use windows once it's added.
+ * @todo Being able to... exit
+ */
 int main_mandelbrot(int argc, char** argv) {
     if (argc != 2) {
         printf("Wrong number of arguments.\n"
@@ -57,6 +71,12 @@ int main_mandelbrot(int argc, char** argv) {
         return 1;
     }
 
+    bool was_kb_echo = kb_getecho();
+    kb_noecho();
+
+    bool was_kb_raw = kb_getraw();
+    kb_raw();
+
     /* Framebuffer. Each 32 bit entry is a color */
     volatile uint32_t* fb = fb_get_ptr();
 
@@ -64,80 +84,128 @@ int main_mandelbrot(int argc, char** argv) {
     const double scaled_h = h / 2.0;
     const double scaled_w = w / 3.0;
 
-    /** @todo Interactive loop */
+    /* Will become smaller when zooming */
+    double zoom     = DEFAULT_ZOOM;
+    double x_offset = DEFAULT_X_OFF;
+    double y_offset = DEFAULT_X_OFF;
 
-    /* Mandelbrot */
-    for (uint32_t y_px = 0; y_px < h; y_px++) {
-        /* Real Y is the mandelbrot center vertically. We subtract 1.0 (half the
-         * height) to center it vertically. */
-        double real_y = (y_px / scaled_h) - 1.0;
-        real_y *= ZOOM;
-        real_y += Y_OFFSET;
+    /* Will be scaled when zooming, so we don't move too much */
+    double move_step = 0.1;
 
-        for (uint32_t x_px = 0; x_px < w; x_px++) {
-            /* Real X is the mandelbrot center horizontally. We subtract 2.0
-             * (half the width) to center it horizontally. */
-            double real_x = (x_px / scaled_w) - 2.0;
-            real_x *= ZOOM;
-            real_x += X_OFFSET;
+    bool main_loop = true;
+    while (main_loop) {
+        /* Mandelbrot */
+        for (uint32_t y_px = 0; y_px < h; y_px++) {
+            /* Real Y is the mandelbrot center vertically. We subtract 1.0 (half
+             * the height) to center it vertically. */
+            double real_y = (y_px / scaled_h) - 1.0;
+            real_y *= zoom;
+            real_y += y_offset;
 
-            /* These 2 values will be increased each iteration bellow */
-            double x = real_x;
-            double y = real_y;
+            for (uint32_t x_px = 0; x_px < w; x_px++) {
+                /* Real X is the mandelbrot center horizontally. We subtract 2.0
+                 * (half the width) to center it horizontally. */
+                double real_x = (x_px / scaled_w) - 2.0;
+                real_x *= zoom;
+                real_x += x_offset;
 
-            bool inside_set = true;
+                /* These 2 values will be increased each iteration bellow */
+                double x = real_x;
+                double y = real_y;
 
-            /* In each iteration, we will check if we are inside the mandebrot
-             * set. An interesting property of the mandelbrot set is that the
-             * more iterations an outside value takes, the closer to the set is.
-             * We can use this to change colors. Ouside of the loop. */
-            int iter;
-            for (iter = 0; iter < max_iter; iter++) {
-                /* Calulate squares once */
-                double sqr_x = x * x;
-                double sqr_y = y * y;
+                bool inside_set = true;
 
-                /* Absolute value of a complex number is the distance from
-                 * origin: sqrt(x^2 + y^2) > 2 */
-                if ((sqr_x + sqr_y) > 2 * 2) {
-                    inside_set = false;
-                    break;
+                /* In each iteration, we will check if we are inside the
+                 * mandebrot set. An interesting property of the mandelbrot set
+                 * is that the more iterations an outside value takes, the
+                 * closer to the set is. We can use this to change colors.
+                 * Ouside of the loop. */
+                int iter;
+                for (iter = 0; iter < max_iter; iter++) {
+                    /* Calulate squares once */
+                    double sqr_x = x * x;
+                    double sqr_y = y * y;
+
+                    /* Absolute value of a complex number is the distance from
+                     * origin: sqrt(x^2 + y^2) > 2 */
+                    if ((sqr_x + sqr_y) > 2 * 2) {
+                        inside_set = false;
+                        break;
+                    }
+
+                    /* This part is explained in the povusers.org link on
+                     * credits */
+                    y = (2.0 * x * y) + real_y;
+                    x = (sqr_x - sqr_y) + real_x;
                 }
 
-                /* This part is explained in the povusers.org link on credits */
-                y = (2.0 * x * y) + real_y;
-                x = (sqr_x - sqr_y) + real_x;
+                /* If it's inside the set, draw fixed color */
+                if (inside_set) {
+                    fb[y_px * w + x_px] = INSIDE_COL;
+                    continue;
+                }
+
+                /* Get 0..360 hue for color based on iter..max_iter
+                 *
+                 * NOTE: Make sure you change the variable type to double if you
+                 * want to scale the Saturation or Value instead (to not
+                 * truncate to zero) */
+                int scaled_hue = iter * COL_H / max_iter;
+
+                float fl_r, fl_g, fl_b;
+                hsv2rgb(&fl_r, &fl_g, &fl_b, scaled_hue, COL_S, COL_V);
+
+                /* NOTE: Try to replace some of these parameters with zeros or
+                 * mess with the scaling and see what happens :) */
+                int r = fl_r * 255.f;
+                int g = fl_g * 255.f;
+                int b = fl_b * 255.f;
+
+                fb[y_px * w + x_px] = rgb2col(r, g, b);
             }
+        }
 
-            /* If it's inside the set, draw fixed color */
-            if (inside_set) {
-                fb[y_px * w + x_px] = INSIDE_COL;
-                continue;
-            }
-
-            /* Get 0..360 hue for color based on iter..max_iter
-             *
-             * NOTE: Make sure you change the variable type to double if you
-             * want to scale the Saturation or Value instead (to not truncate to
-             * zero) */
-            int scaled_hue = iter * COL_H / max_iter;
-
-            float fl_r, fl_g, fl_b;
-            hsv2rgb(&fl_r, &fl_g, &fl_b, scaled_hue, COL_S, COL_V);
-
-            /* NOTE: Try to replace some of these parameters with zeros or mess
-             * with the scaling and see what happens :) */
-            int r = fl_r * 255.f;
-            int g = fl_g * 255.f;
-            int b = fl_b * 255.f;
-
-            fb[y_px * w + x_px] = rgb2col(r, g, b);
+        /* Get user input */
+        switch (getchar()) {
+            case KEY_ZOOM_IN:
+                zoom *= ZOOM_STEP;
+                move_step *= ZOOM_STEP;
+                break;
+            case KEY_ZOOM_OUT:
+                zoom /= ZOOM_STEP;
+                move_step /= ZOOM_STEP;
+                break;
+            case KEY_UP:
+                if (y_offset > -1.1)
+                    y_offset -= move_step;
+                break;
+            case KEY_DOWN:
+                if (y_offset < 1.1)
+                    y_offset += move_step;
+                break;
+            case KEY_LEFT:
+                if (x_offset > -2.1)
+                    x_offset -= move_step;
+                break;
+            case KEY_RIGHT:
+                if (x_offset < 2.1)
+                    x_offset += move_step;
+                break;
+            case KEY_RESET:
+                zoom     = DEFAULT_ZOOM;
+                x_offset = DEFAULT_X_OFF;
+                y_offset = DEFAULT_X_OFF;
+                break;
+            default:
+                break;
         }
     }
 
-    /* FIXME */
-    for (;;)
-        ;
+    if (was_kb_echo)
+        kb_echo();
+
+    if (!was_kb_raw)
+        kb_noraw();
 
     return 0;
 }
