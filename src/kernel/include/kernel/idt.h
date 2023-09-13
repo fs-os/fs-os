ab7e4061614cf51fcc4bb0bc4dde2055bb058de0
8dcc <8dcc.git@gmail.com>
2023-09-13 19:02:41 +0200

    Improve idt.h comments, change spacing

--- src/kernel/include/kernel/idt.h
+++ src/kernel/include/kernel/idt.h
@@ -32,11 +32,11 @@ enum idt_dpl {
  * For more information, see: https://wiki.osdev.org/IDT#Gate_Types
  */
 enum idt_gate_types {
-    IDT_GATE_TASK       = 0x5, /**< @brief Task gate. Unused */
-    IDT_GATE_16BIT_INT  = 0x6, /**< @brief 16 bit interrupt. Unused */
-    IDT_GATE_16BIT_TRAP = 0x7, /**< @brief 16 bit trap. Unused */
-    IDT_GATE_32BIT_INT  = 0xE, /**< @brief 32 bit interrupt */
-    IDT_GATE_32BIT_TRAP = 0xF, /**< @brief 32 bit trap */
+    IDT_GATE_TASK       = 0x5, /* Task gate. Unused */
+    IDT_GATE_16BIT_INT  = 0x6, /* 16 bit interrupt. Unused */
+    IDT_GATE_16BIT_TRAP = 0x7, /* 16 bit trap. Unused */
+    IDT_GATE_32BIT_INT  = 0xE, /* 32 bit interrupt */
+    IDT_GATE_32BIT_TRAP = 0xF, /* 32 bit trap */
 };
 
 /**
@@ -47,10 +47,10 @@ enum idt_gate_types {
  * See https://wiki.osdev.org/PIC
  */
 enum pic_ports {
-    PIC_MASTER_CMD  = 0x20, /**< @brief Command I/O port of the master PIC */
-    PIC_MASTER_DATA = 0x21, /**< @brief Data I/O port of the master PIC */
-    PIC_SLAVE_CMD   = 0xA0, /**< @brief Command I/O port of the slave PIC */
-    PIC_SLAVE_DATA  = 0xA1, /**< @brief Data I/O port of the slave PIC */
+    PIC_MASTER_CMD  = 0x20, /* Command I/O port of the master PIC */
+    PIC_MASTER_DATA = 0x21, /* Data I/O port of the master PIC */
+    PIC_SLAVE_CMD   = 0xA0, /* Command I/O port of the slave PIC */
+    PIC_SLAVE_DATA  = 0xA1, /* Data I/O port of the slave PIC */
 };
 
 /**
@@ -58,17 +58,17 @@ enum pic_ports {
  * @brief Flags used for the data and command pic_ports
  */
 enum pic_flags {
-    ICW1_ICW4      = 0x01, /**< @brief ICW4 (not) needed */
-    ICW1_SINGLE    = 0x02, /**< @brief Single (cascade) mode */
-    ICW1_INTERVAL4 = 0x04, /**< @brief Call address interval 4 (8) */
-    ICW1_LEVEL     = 0x08, /**< @brief Level triggered (edge) mode */
-    ICW1_INIT      = 0x10, /**< @brief Initialization - required! */
+    ICW1_ICW4      = 0x01, /* ICW4 (not) needed */
+    ICW1_SINGLE    = 0x02, /* Single (cascade) mode */
+    ICW1_INTERVAL4 = 0x04, /* Call address interval 4 (8) */
+    ICW1_LEVEL     = 0x08, /* Level triggered (edge) mode */
+    ICW1_INIT      = 0x10, /* Initialization - required! */
 
-    ICW4_8086       = 0x01, /**< @brief 8086/88 (MCS-80/85) mode */
-    ICW4_AUTO       = 0x02, /**< @brief Auto (normal) EOI */
-    ICW4_BUF_SLAVE  = 0x08, /**< @brief Buffered mode/slave */
-    ICW4_BUF_MASTER = 0x0C, /**< @brief Buffered mode/master */
-    ICW4_SFNM       = 0x10, /**< @brief Special fully nested (not) */
+    ICW4_8086       = 0x01, /* 8086/88 (MCS-80/85) mode */
+    ICW4_AUTO       = 0x02, /* Auto (normal) EOI */
+    ICW4_BUF_SLAVE  = 0x08, /* Buffered mode/slave */
+    ICW4_BUF_MASTER = 0x0C, /* Buffered mode/master */
+    ICW4_SFNM       = 0x10, /* Special fully nested (not) */
 };
 
 /**
@@ -82,23 +82,34 @@ enum pic_flags {
  * See https://wiki.osdev.org/IDT
  */
 typedef struct {
-    uint16_t offset_l; /**< @brief Bits 0..15 of the offset. The offset is
-                        * relative to the selected GDT segment. */
-    uint16_t selector; /**< @brief Code segment selector of the GDT (or LDT) */
-    uint8_t zero;      /**< @brief Unused, 0 */
-    uint8_t type; /**< @brief Gate type, DPL (permissions), "Present" field */
-    uint16_t offset_h; /**< @brief Bits 16..31 of the offset */
+    /** @brief Bits 0..15 of the offset. The offset is relative to the current
+     * GDT segment. */
+    uint16_t offset_l;
+
+    /** @brief Code segment selector of the GDT (or LDT) */
+    uint16_t selector;
+
+    /** @brief Unused, 0 */
+    uint8_t zero;
+
+    /** @brief Gate Type, DPL (permissions) and Present flag */
+    uint8_t type;
+
+    /** @brief Bits 16..31 of the offset */
+    uint16_t offset_h;
 } __attribute__((packed)) idt_entry;
 
 /**
- * @struct __attribute__
- * @brief Actual idt descriptor structure needed by the `lidt` assembly
- * instruction.
- * @details Same as the gdt descriptor from src/kernel/gdt.asm
+ * @struct idt_descriptor
+ * @brief Actual IDT descriptor structure needed by the LIDT asm instruction.
+ * @details Same as the GDT descriptor from src/kernel/gdt.asm
  * */
 typedef struct {
-    uint16_t limit;  /**< @brief Size of the IDT minus 1 */
-    idt_entry* base; /**< @brief Address of the IDT itself */
+    /** @brief Size of the IDT minus 1 */
+    uint16_t limit;
+
+    /** @brief Address of the IDT itself */
+    idt_entry* base;
 } __attribute__((packed)) idt_descriptor;
 
 /**
