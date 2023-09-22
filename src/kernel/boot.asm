7f2a41578854efd6ad1cb32bf8a9a7b0468fe30d
8dcc <8dcc.git@gmail.com>
2023-09-22 14:05:22 +0200

    Format comments of boot.asm

--- src/kernel/boot.asm
+++ src/kernel/boot.asm
@@ -16,21 +16,10 @@ MB_MAGIC    equ 0x1BADB002
 ; Checksum of above to prove we are multiboot.
 MB_CHECKSUM equ -(MB_MAGIC + MB_FLAGS)
 
-; Declare the multiboot header that marks the program as a kernel. These are
-; magic values that are documented in the multiboot standard. The bootloader
-; will search for this signature in the first 8KiB of the kernel file, aligned
-; at a 32-bit boundary.
-; Here we ask for graphics mode type 0, which is linear type. We use 0 as width
-; and height because in this case we don't care about a "default resolution",
-; we want to use what the bootloader gives us. Depth will be the amount of
-; bytes per pixel.
-;
-; More information on alignment:
-;   https://stackoverflow.com/questions/19608845/understanding-assembly-mips-align-and-memory-addressing
-; More information on next values & credits:
-;   https://www.gnu.org/software/grub/manual/multiboot/html_node/Header-address-fields.html#Header-address-fields
-;   https://www.gnu.org/software/grub/manual/multiboot/html_node/Header-graphics-fields.html#Header-graphics-fields
-;   https://github.com/ozkl/soso/blob/master/kernel/boot.asm#L17-L26
+; Declare the multiboot header. These are magic values that are documented in
+; the multiboot standard. The bootloader will search for this signature in the
+; first 8KiB of the kernel file, aligned at a 32-bit boundary.
+; For more information on these values, see the wiki.
 section .multiboot
     align 4
     dd      MB_MAGIC
@@ -46,43 +35,33 @@ section .multiboot
     dd      0x00000000      ; (graphics) height
     dd      32              ; (graphics) depth
 
-; Create a symbol at the bottom of the stack, allocate 16384 bytes and create
-; another symbol at the top. Keep in mind that the stack grows downwards in
-; x86. The stack is in its own section because in nasm, .bss is marked as
-; nobits which means the kernel file is smaller, because it does not contain an
-; initialized stack.
-; From https://www.nasm.us/doc/nasmdoc8.html
-;   > nobits defines the section to be one with no explicit contents given,
-;   > such as a BSS section.
-; The stack must be 16 byte aligned acording to the System V ABI. The compiler
-; will assume the stack is properly aligned and failure to align the stack will
-; result in undefined behavior.
+; Allocate 16KiB for the main kernel stack, add labels to indicate where it
+; starts and ends. The stack must be 16 byte aligned acording to the System V
+; ABI. Keep in mind that the stack grows downwards in x86. The stack is in .bss
+; so it's not contained in the binary.
 section .bss
     global stack_bottom     ; Global used in src/kernel/multitask.asm
     global stack_top
 
-    align   16
+    align 16
     stack_bottom:       ; Items pushed to the stack will get 'closer' to this.
         resb    0x4000  ; 16 KiB
     stack_top:
 
 ; Declare the kernel entry point.
 section .text
-    global _start:function (_start.end - _start)
-    extern kernel_main                              ; src/kernel/kernel.c
-    extern gdt_init                                 ; src/kernel/gdt.asm
-    extern is_sse_supported                         ; src/kernel/util.asm
-    extern sse_supported                            ; src/kernel/kernel.c
-    extern is_msr_supported                         ; src/kernel/util.asm
-    extern msr_supported                            ; src/kernel/kernel.c
-    extern is_tsc_supported                         ; src/kernel/util.asm
-    extern tsc_supported                            ; src/kernel/kernel.c
+    extern kernel_main                  ; src/kernel/kernel.c
+    extern gdt_init                     ; src/kernel/gdt.asm
+    extern is_sse_supported             ; src/kernel/util.asm
+    extern sse_supported                ; src/kernel/kernel.c
+    extern is_msr_supported             ; src/kernel/util.asm
+    extern msr_supported                ; src/kernel/kernel.c
+    extern is_tsc_supported             ; src/kernel/util.asm
+    extern tsc_supported                ; src/kernel/kernel.c
 
+global _start:function
 _start:
-    ; The bootloader loaded us into 32bit protected mode on a x86 machine.
-    ; Interrupts are disabled. Paging is disabled. The processor state is as
-    ; defined in the multiboot standard. There is no stdlib. There are no
-    ; security restrictions, safeguards or debugging mechanisms.
+    ; The bootloader just loaded us into 32-bit protected mode.
 
     ; To setup the stack, we move esp to the 'top' label we declared earlier,
     ; because the stack grows to a lower memory address in x86 assembly.
@@ -156,6 +135,4 @@ _start:
 .hang:
     hlt
     jmp     .hang
-
-.end:
 
