57677d157ed41702507f4d404a511a07f8531ccc
8dcc <8dcc.git@gmail.com>
2023-10-09 23:31:56 +0200

    Add isalpha, isdigit, isalnum and isblank to ctype.h
    Fix typos

--- src/libk/include/ctype.h
+++ src/libk/include/ctype.h
@@ -5,35 +5,71 @@
 #include <stdbool.h>
 
 /**
- * @brief Returns true if `c` is uppercase.
- * @param[in] c Character to check
- * @return True of uppercase
- */
-static inline bool isupper(int c) {
-    return c >= 'A' && c <= 'Z';
-}
-
-/**
- * @brief Returns true if `c` is lowercase.
- * @param[in] c Character to check
- * @return True of lowercase
- */
-static inline bool islower(int c) {
-    return c >= 'a' && c <= 'z';
-}
-
-/**
- * @brief If c is uppercase, returns its lowercase version.
+ * @brief If c is uppercase, returns its lowercase version
  * @param[in] c Uppercase char
  * @return Lowercase version of c
  */
 int tolower(int c);
 
 /**
- * @brief If c is lowercase, returns its uppercase version.
+ * @brief If c is lowercase, returns its uppercase version
  * @param[in] c Lowercase char
  * @return Uppercase version of c
  */
 int toupper(int c);
+
+/**
+ * @brief Returns true if `c` is uppercase
+ * @param[in] c Character to check
+ * @return True if uppercase
+ */
+static inline bool isupper(int c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+/**
+ * @brief Returns true if `c` is lowercase
+ * @param[in] c Character to check
+ * @return True if lowercase
+ */
+static inline bool islower(int c) {
+    return c >= 'a' && c <= 'z';
+}
+
+/**
+ * @brief Returns true if `c` is an alphabetic character
+ * @param[in] c Character to check
+ * @return True if digit
+ */
+static inline bool isalpha(int c) {
+    return isupper(c) || islower(c);
+}
+
+/**
+ * @brief Returns true if `c` is a digit
+ * @param[in] c Character to check
+ * @return True if digit
+ */
+static inline bool isdigit(int c) {
+    return c >= '0' && c <= '9';
+}
+
+/**
+ * @brief Returns true if `c` is alphanumeric
+ * @param[in] c Character to check
+ * @return True if alphanumeric
+ */
+static inline bool isalnum(int c) {
+    return isalpha(c) || isdigit(c);
+}
+
+/**
+ * @brief Returns true if `c` is tab or space
+ * @param[in] c Character to check
+ * @return True if blank character
+ */
+static inline bool isblank(int c) {
+    return c == ' ' || c == '\t';
+}
 
 #endif /* _CTYPE_H */
