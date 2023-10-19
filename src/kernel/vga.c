a878c161835d4907d3cc500d37420dc408baa95c
8dcc <8dcc.git@gmail.com>
2023-10-19 15:39:44 +0200

    Improve comments of vga.c, remove VGA_*

--- src/kernel/vga.c
+++ src/kernel/vga.c
@@ -2,28 +2,29 @@
 #include <string.h>
 #include <kernel/vga.h>
 
-const size_t VGA_WIDTH  = 80;
-const size_t VGA_HEIGHT = 25;
+/** @brief Current position in the VGA console */
+static size_t term_y = 0, term_x = 0;
 
-static size_t term_y;      /**< @brief Current Y position in the VGA console */
-static size_t term_x;      /**< @brief Current X position in the VGA console */
-static uint8_t term_col;   /**< @brief Color */
-static uint16_t* term_buf; /**< @brief Buffer of vga_entries */
+/** @brief Current color from when for drawing in VGA console. See vga_color */
+static uint8_t term_col = 0;
+
+/** @brief Pointer to the vga_entry() buffer located at VGA_CONSOLE_ADDR */
+static uint16_t* term_buf = NULL;
+
+/*----------------------------------------------------------------------------*/
 
 void vga_init(void) {
     term_y   = 0;
     term_x   = 0;
     term_col = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    /* 0xB8000 is the location of the VGA text mode buffer */
+
+    /* Defined in vga.h, 0xB8000 is the location of the VGA text mode buffer */
     term_buf = (uint16_t*)VGA_CONSOLE_ADDR;
 
     /* Clear terminal buffer */
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; x++) {
-            /* 2D coordinates to 1D array idx */
+    for (int y = 0; y < VGA_HEIGHT; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
             term_buf[y * VGA_WIDTH + x] = vga_entry(' ', term_col);
-        }
-    }
 }
 
 void vga_setcol_entry(uint8_t col) {
@@ -40,13 +41,13 @@ void vga_put_at(size_t y, size_t x, uint8_t col, char c) {
 
 void vga_shift_rows(int n) {
     /* Shift n rows */
-    for (size_t y = 0; y < VGA_HEIGHT - n; y++)
-        for (size_t x = 0; x < VGA_WIDTH; x++)
+    for (int y = 0; y < VGA_HEIGHT - n; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
             term_buf[y * VGA_WIDTH + x] = term_buf[(y + n) * VGA_WIDTH + x];
 
     /* Clear last n rows with the current color (only the background matters) */
-    for (size_t y = VGA_HEIGHT - n; y < VGA_HEIGHT; y++)
-        for (size_t x = 0; x < VGA_WIDTH; x++)
+    for (int y = VGA_HEIGHT - n; y < VGA_HEIGHT; y++)
+        for (int x = 0; x < VGA_WIDTH; x++)
             term_buf[y * VGA_WIDTH + x] = vga_entry(' ', term_col);
 }
 
